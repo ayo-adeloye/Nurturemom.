e514042e1909874e3f84016d000124b4ffd6a71f
ayo-adeloye <adekunlasia@gmail.com>
2026-09-17 10:56:02 -0400

    Load automatic movement tracker in Plus preview

--- worker-plus.js
+++ worker-plus.js
@@ -7,7 +7,7 @@ export default {
       return new HTMLRewriter()
         .on('body', {
           element(element) {
-            element.append('<script>window.NURTUREMOM_PLUS_PREVIEW=true;</script><script src="/original-approved-ux.js"></script><script src="/plus-access.js"></script><script src="/plus-options.js"></script><script src="/plus-account-ui.js"></script>', { html: true });
+            element.append('<script>window.NURTUREMOM_PLUS_PREVIEW=true;</script><script src="/original-approved-ux.js"></script><script src="/plus-access.js"></script><script src="/plus-options.js"></script><script src="/plus-auto-movement.js"></script><script src="/plus-account-ui.js"></script>', { html: true });
           }
         })
         .transform(response);
